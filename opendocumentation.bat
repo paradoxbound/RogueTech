133d9f933a7f49e05c90ad04bf5c9dc7c54a7cdc
LadyAlekto <ladyalekto@hotmail.de>
2018-07-09 05:59:13 +0200

    987 Preview4

--- opendocumentation.bat
+++ opendocumentation.bat
@@ -1,3 +1,16 @@
 @echo off
-echo Open Readme link
+COLOR 0C
+title Readme Opener
+echo Hit enter to open the Readmes
+echo -
+echo ! RTFM !
+echo -
+echo The Articles are the Manual
+timeout /t 30
 start "" https://www.nexusmods.com/battletech/mods/79?tab=articles
+pause
+cls
+echo .
+echo SERIOUSLY READ THEM
+echo .
+pause
